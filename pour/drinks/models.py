c6d6d979bfeccf8572047fde59dcd09d871128af
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-03 19:13:34 -0400

    Models, drinks, Ingredients

--- pour/drinks/models.py
+++ pour/drinks/models.py
@@ -1,3 +1,17 @@
+from operator import imod
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Ingredients(models.Model):
+    ingredient = models.CharField(max_length=200)
+
+class Drinks(models.Model):
+    drink_name = models.CharField(max_length=200)
+    drink_ingredients= ArrayField(models.CharField(max_length=100), default = [])
+    garnishes = ArrayField(models.CharField(max_length=100), default = [])
+    directions= models.CharField(max_length=2000)
+    glass = models.CharField(max_length=200)
+    tools = ArrayField(models.CharField(max_length=100), default = [])
+    tags = ArrayField(models.CharField(max_length=100), default = [])
+
--- pour/drinks/views.py
+++ pour/drinks/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
+from pour.drinks.models import Drinks, Ingredients
 
 def home(request):
-  template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+    mying = Ingredients.objects.all().values
+    mydrinks = Drinks.objects.all().values
+    template = loader.get_template('home.html')  
+    context = {
+        'mying' : mying,
+        'mydrinks' : mydrinks
+    }
+    return HttpResponse(template.render(context, request))
+
+def index(request):
+    mydrinks = Drinks.objects.all().values
+    template = loader.get_template('index.html')
+    context = {
+        'mydrinks' : mydrinks
+    }
+    return HttpResponse(template.render(context, request))
