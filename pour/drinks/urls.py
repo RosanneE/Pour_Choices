e0485842eb3d50fb59feaa9a6084ee54fa04e429
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-06 19:46:42 -0400

    Create and Show paths, Create page

--- pour/drinks/urls.py
+++ pour/drinks/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('create/', views.DrinksCreate.as_view(), name='create/'),
+    path('index/', views.Index.as_view(), name='index'),
+    path('cocktail/<int:pk>/', views.CocktailShow.as_view(), name='cocktail_show'),
     path('ing_search/', views.ing_search, name='ingredients'),
 ]
--- pour/drinks/views.py
+++ pour/drinks/views.py
@@ -2,8 +2,10 @@ from multiprocessing import context
 from pickle import NONE
 from unicodedata import name
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.views.generic import DetailView
 from django.urls import reverse
 
 from drinks.models import Drinks, Ingredients
@@ -17,12 +19,36 @@ class Home(TemplateView):
         name = self.request.GET.get("ingredient")
         # If param, will filter by param
         if name != None:
-            context['ingredients'] = Ingredients.objects.filter(ingredient__icontains=name).values
+            context['ingredients'] = Ingredients.objects.filter(ingredient__icontains=name).values()
             context["header"] = f'Ingredients containing "{name}"'
         else:
-            context['ingredients'] = Ingredients.objects.all().values
+            context['ingredients'] = Ingredients.objects.all().values()
             context["header"] = "Ingredients"
         return context
+
+class Index(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        # Get gets search param
+        name = self.request.GET.get("drink_name")
+        if name != None:
+            context['drinks'] = Drinks.objects.filter(drink_name__icontains=name).values()
+            context["headerC"] = f'Cocktails containing "{name}"'
+        else:
+            context['drinks'] = Drinks.objects.all().values()
+        return context
+
+class DrinksCreate(CreateView):
+    model = Drinks
+    fields = ['drink_name','drink_ingredients']
+    template_name = 'createCocktail.html'
+    success_url = '/create/'
+
+class CocktailShow(DetailView):
+    model = Drinks
+    template_name = 'cocktail_show'
 
 def about(request):
     template = loader.get_template('about.html')
@@ -37,11 +63,5 @@ def ing_search(request):
     }
     return HttpResponse(template.render(context, request))
 
-def index(request):
-    mydrinks = Drinks.objects.all().values
-    template = loader.get_template('index.html')
-    context = {
-        'mydrinks' : mydrinks
-    }
-    return HttpResponse(template.render(context, request))
+
 
