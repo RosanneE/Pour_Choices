3a1fffae9b115c081785b6df18ab23044642fff0
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-07 20:44:37 -0400

    initialization of many to many

--- pour/drinks/admin.py
+++ pour/drinks/admin.py
@@ -2,4 +2,4 @@ from typing import List
 from django.contrib import admin
 from .models import Ingredients, Drinks
 # Register your models here.
-admin.site.register(Ingredients)
+# admin.site.register(Ingredients)
