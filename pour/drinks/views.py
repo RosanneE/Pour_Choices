00912730f85dd06df7e2b1ac89232e092b854a67
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-08 13:56:15 -0400

    account page

--- pour/drinks/views.py
+++ pour/drinks/views.py
@@ -7,11 +7,10 @@ from django.template import loader
 from django.views.generic import DetailView
 from django.urls import reverse
 from pickle import NONE
-from drinks.models import Drinks, Ingredients
+from drinks.models import Drinks, Ingredients, Lists
 
 class Home(TemplateView):
     template_name = "home.html"
-
     def get_context_data(self, **kwargs): 
         context = super().get_context_data(**kwargs)
         # Get gets search param
@@ -27,7 +26,6 @@ class Home(TemplateView):
 
 class Index(TemplateView):
     template_name = "index.html"
-
     def get_context_data(self, **kwargs): 
         context = super().get_context_data(**kwargs)
         # Get gets search param
@@ -43,7 +41,9 @@ class DrinksCreate(CreateView):
     model = Drinks
     fields = ['drink_name','drink_ingredients', 'recipie', 'glass', 'tags']
     template_name = 'create_cocktail.html'
-    success_url = '/create/'
+    def get_success_url(self):
+        return reverse('cocktail_show', kwargs={'pk': self.object.pk})
+
 
 class CocktailShow(DetailView):
     model = Drinks
@@ -53,10 +53,16 @@ class CocktailUpdate(UpdateView):
     model = Drinks
     fields = ['drink_name','drink_ingredients', 'recipie', 'glass', 'tags']
     template_name = 'cocktail_update.html'
-
+    # success_url = '/index/'
     def get_success_url(self):
         return reverse('cocktail_show', kwargs={'pk': self.object.pk})
 
+class MyPage(TemplateView):
+    template_name = "my_page.html"
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context['lists'] = Lists.objects.all().values()
+        return context
 
 def about(request):
     template = loader.get_template('about.html')
