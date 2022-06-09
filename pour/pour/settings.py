b0bb8e39db239bbf309c10d4fc557689cfcca76e
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-09 12:01:10 -0400

    django heroku update11

--- pour/pour/settings.py
+++ pour/pour/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [BASE_DIR / 'pour/static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
