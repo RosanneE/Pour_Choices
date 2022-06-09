e2fc93e806948617e892279d870fc49867b21155
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-09 11:53:51 -0400

    django heroku update7

--- pour/pour/settings.py
+++ pour/pour/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/pour/drinks/static/',
+    '/pourapp/static/',
 ]
 
 # Default primary key field type
