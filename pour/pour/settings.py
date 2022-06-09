53b572ddcb58adcf98db0a99da7cff991b3744c8
rosanne-a <anderson.rosanne4@gmail.com>
2022-06-09 11:37:49 -0400

    django heroku update1

--- pour/pour/settings.py
+++ pour/pour/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", '/pour/drinks/static/',]
 # [os.path.join(BASE_DIR,'/pour/drinks/static/')]
-# STATIC_ROOT = os.path.join(BASE_DIR, '/pour/drinks/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
